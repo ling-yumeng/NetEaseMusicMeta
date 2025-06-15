fdfbf4c98a06a71a90d689e90a8d3897230d52ce
MapleLeaf <197728340@qq.com>
2025-06-15 09:18:56 +0800

    refactor(index): 重构代码并添加搜索功能
    - 注释掉原有的 getDetail 和 readMetadata 代码块
    - 添加搜索功能，使用 search 函数查找歌曲 ID
    - 示例中搜索了"创作者之死"并打印了详细信息

--- index.ts
+++ index.ts
@@ -1,16 +1,20 @@
 import getLyric from "@models/netease/lyric";
 import getDetail from "@models/netease/detail";
+import search from "@models/netease/search";
 import writeMetadataWithFFmpeg from "@utils/ffmpeg/writeMetadata";
 import readMetadata from "@utils/ffmpeg/readMetadata";
-let id = 2034361036
 
-getDetail(id).then(async (detail) => {
-  if (!detail) return;
-  writeMetadataWithFFmpeg(
-    "/home/ml/下载/64Gram Desktop/免我蹉跎苦 (Single Version)-阿萨Aza.ogg.mp3",
-    detail,
-    await getLyric(id)
-  );
-});
+// getDetail(id).then(async (detail) => {
+//   if (!detail) return;
+//   writeMetadataWithFFmpeg(
+//     "/home/ml/下载/64Gram Desktop/免我蹉跎苦 (Single Version)-阿萨Aza.ogg.mp3",
+//     detail,
+//     await getLyric(id)
+//   );
+// });
 
 //readMetadata("/home/ml/下载/64Gram Desktop/免我蹉跎苦 (Single Version)-阿萨Aza.ogg.mp3").then(console.log)
+
+const id = await search("创作者之死") as any
+
+console.log(await getDetail(id.id))
---
+++ models/netease/search.ts
@@ -0,0 +1,27 @@
+import sign from "./sign";
+import jsonToUrlEncoded from "@utils/jsonToUrlEncoded";
+
+export default async function search(keywords: string) {
+    let e = {
+        "s": String(keywords),
+        "limit": "1",
+        "csrf_token": ""
+    }
+
+    const { encText, encSecKey } = await sign(e);
+
+    const body = jsonToUrlEncoded({
+        params: encText,
+        encSecKey
+    });
+
+    const response = await fetch("https://music.163.com/weapi/search/suggest/web?csrf_token=", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body,
+    });
+    const data = await response.json() as any
+    return data.result.songs[0];
+}

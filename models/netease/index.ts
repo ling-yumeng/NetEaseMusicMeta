99baded513eaa4fac9f2367ac568d5f8d7b7e51f
MapleLeaf <197728340@qq.com>
2025-06-15 10:03:56 +0800

    refactor(netease): 移除未使用的 sign 模块导入
    - 删除了 netease 模型中未使用的 sign 模块导入语句
    - 优化代码结构，提高代码的可读性和维护性

--- models/netease/index.ts
+++ models/netease/index.ts
@@ -1,10 +1,8 @@
-import sign from "./sign";
 import getDetail from "./detail";
 import getLyric from "./lyric";
 import search from "./search";
 
 export {
-    sign,
     getDetail,
     getLyric,
     search
